49b3e0e947419b2aba3f42171e6b016f244def7a
Md Nayeem Farid <mdnayeemfarid@Md-MacBoo-k-Air.local>
2022-02-11 01:02:49 +0600

    Create pizza-pool.php

---
+++ pizza-pool.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+* Plugin Name: Pizza pool
+* Description: Pizza Pool is a WooCommerce extention to make aditional delivery option for the resturent  
+* Author: Md Nayeem Farid
+* Author URI: https://mdnayeemfarid.com
+* Plugin URI: https://mdnayeemfarid.com/pizza-pool
+* Version: 1.0
+* Text Domain: pizza-pool
+*/
+
+
+
+
