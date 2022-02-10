0940788849620b6102e3e7bda0486d2369f0a601
Md Nayeem Farid <mdnayeemfarid@Md-MacBoo-k-Air.local>
2022-02-11 02:06:41 +0600

    Optimize delivery system

--- inc/classes/class-delivery.php
+++ inc/classes/class-delivery.php
@@ -8,12 +8,19 @@ class delivery {
 
         add_filter('woocommerce_order_button_html', [$this, 'should_display_place_order_button'] );
         add_action( 'woocommerce_after_checkout_billing_form', [$this, 'add_dilavery_option_fields'] );
-        add_action( 'woocommerce_cart_calculate_fees', [$this, 'woo_add_cart_fee'] );
-        add_action( 'woocommerce_cart_calculate_fees', [$this, 'prefix_add_discount_line'] );
+        add_action( 'woocommerce_cart_calculate_fees', [$this, 'add_fee_for_deliver_optons'] );
+        add_action( 'woocommerce_cart_calculate_fees', [$this, 'user_first_order_discount'] );
 
     }
 
-    public function should_display_place_order_button($button) {
+    /**
+     * should_display_place_order_button
+     *
+     * @param [type] $button
+     * @return default woocommerce place order button or open and close time for pizza pool 
+     */
+
+    public function should_display_place_order_button( $button ) {
         $is_open =  new \pizza\pool\schedules;
         if($is_open->is_pizza_pool_open() == 'open'){
             return $button;
@@ -22,7 +29,14 @@ class delivery {
         }
       
     }
-    public function add_dilavery_option_fields($checkout) {
+    /**
+     * add_dilavery_option_fields
+     *
+     * @param [string] $checkout
+     * Add new fields on woocommerce checkout page to change delivery options
+     */
+
+    public function add_dilavery_option_fields( $checkout ) {
         echo '<div id="pizza-pool-delivery-options-filds">';
         woocommerce_form_field( 'pool-delivery-option', [
             'type'          => 'select',
@@ -38,7 +52,14 @@ class delivery {
             $checkout->get_value( 'pool-delivery-option' ));
 	    echo '</div>';
     }
-    function woo_add_cart_fee( $cart ){
+
+    /**
+     * add_fee_for_deliver_optons
+     * if should add extra change for deliver then add it 
+     */
+
+    function add_fee_for_deliver_optons( $cart ){
+      
         if ( ! $_POST || ( is_admin() && ! is_ajax() ) ) {
             return;
         }
@@ -66,29 +87,40 @@ class delivery {
     
     }
 
-    // get first order 
+    /**
+     * is_user_first_order
+     * Check user ordering first time 
+     * @return bool
+     */
 
     public function is_user_first_order () {
-        // Get all customer orders
-    $customer_orders = get_posts( array(
-        'numberposts' => 1, // one order is enough
-        'meta_key'    => '_customer_user',
-        'meta_value'  => get_current_user_id(),
-        'post_type'   => 'shop_order', // WC orders post type
-        'post_status' => 'wc-completed', // Only orders with "completed" status
-        'fields'      => 'ids', // Return Ids "completed"
-    ) );
 
-    // return "true" when customer has already at least one order (false if not)
-     return count($customer_orders) > 0 ? true : false; 
+        // Get all customer orders
+        $customer_orders = get_posts( array(
+            'numberposts' => 1, // one order is enough
+            'meta_key'    => '_customer_user',
+            'meta_value'  => get_current_user_id(),
+            'post_type'   => 'shop_order', // WC orders post type
+            'post_status' => 'wc-completed', // Only orders with "completed" status
+            'fields'      => 'ids', // Return Ids "completed"
+        ) );
+
+        // return "true" when customer has already at least one order (false if not)
+        return count($customer_orders) > 0 ? true : false; 
     }
 
-    function prefix_add_discount_line( $cart ) {
+    /**
+     * user_first_order_discount
+     * if user first order make 40% discount on checkout page 
+     */
+
+    function user_first_order_discount( $cart ) {
 
         if($this->is_user_first_order() || !is_user_logged_in()){
             $discount = $cart->subtotal * 0.4;      
             $cart->add_fee( __( '40% Discount for first order', 'pizza-pool' ) , -$discount );
-        }      
+        } 
+
       }
     
 } 
